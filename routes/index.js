dfefed61d1c0db75c3ca7993566d05981b957073
CamaradaAnCap <eric.reis@usp.br>
2020-04-01 17:56:15 -0300

    login

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ let router = express.Router();
 let homeController = require('../controllers/homeController');
 let adminController = require('../controllers/adminController');
 let cadastroController = require('../controllers/cadastroController');
+let loginController = require('../controllers/loginController');
 
 /* GET home page. */
 router.get('/', homeController.index);
@@ -11,6 +12,9 @@ router.get('/', homeController.index);
 router.post('/contato', homeController.contato);
 
 router.get('/admin', adminController.admin);
+
+router.get('/login', loginController.cadastro);
+//router.post('/cadastroUsuario', cadastroController.salvar);
 
 router.get('/cadastroUsuario', cadastroController.cadastro);
 router.post('/cadastroUsuario', cadastroController.salvar);
